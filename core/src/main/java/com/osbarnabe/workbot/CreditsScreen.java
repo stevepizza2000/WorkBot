0c5379440641541528c1cef1e8175c63ee5158a4
ArturDCosta <arturdenicolcosta@gmail.com>
2026-05-20 15:48:55 -0300

    Creditos em outras linguas e nome de todo mundo em tudo

--- core/src/main/java/com/osbarnabe/workbot/CreditsScreen.java
+++ core/src/main/java/com/osbarnabe/workbot/CreditsScreen.java
@@ -1,4 +1,5 @@
 package com.osbarnabe.workbot;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
@@ -23,19 +24,58 @@ public class CreditsScreen implements Screen {
 
     private Texture btnVoltar;
     private Texture btnVoltarSelect;
+
     private boolean botaoSelecionado = false;
 
-    private float rolagemY           = -200f;
+    private float rolagemY = -200f;
     private final float velocidadeRolagem = 150f;
-    private final float limiteRolagem     = 3450f;
+    private final float limiteRolagem = 4050f;
+
+    // =========================
+    // TEXTOS LOCALIZADOS
+    // =========================
+
+    private String[] cargoCriadores = {
+        "Criadores",
+        "Creadores",
+        "Creators"
+    };
+
+    private String[] cargoArteDesign = {
+        "Arte e Design",
+        "Arte y Diseño",
+        "Art and Design"
+    };
+
+    private String[] cargoProgramacao = {
+        "Programacao",
+        "Programación",
+        "Programming"
+    };
+
+    private String[] cargoRoteiro = {
+        "Roteiro",
+        "Guión",
+        "Script"
+    };
+
+    private String[] cargoAgradecimentos = {
+        "Agradecimentos Especiais",
+        "Agradecimientos Especiales",
+        "Special Thanks"
+    };
 
     public CreditsScreen(Main jogo) {
+
         float largura = Gdx.graphics.getWidth();
-        float altura  = Gdx.graphics.getHeight();
+        float altura = Gdx.graphics.getHeight();
+
         this.jogo = jogo;
-        batch     = new SpriteBatch();
-        camera    = new OrthographicCamera();
-        viewport  = new FitViewport(largura, altura, camera);
+
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(largura, altura, camera);
 
         fonteCargo = new BitmapFont();
         fonteCargo.getData().setScale(2f);
@@ -48,6 +88,7 @@ public class CreditsScreen implements Screen {
 
     @Override
     public void render(float delta) {
+
         ScreenUtils.clear(0f, 0f, 0f, 1f);
 
         // Rolagem automática
@@ -60,93 +101,209 @@ public class CreditsScreen implements Screen {
         boolean esq = Gdx.input.isKeyPressed(Input.Keys.LEFT);
         boolean dir = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 
-        // Acelera rolagem ao segurar qualquer botão
+        // Acelera rolagem
         if ((esq || dir) && rolagemY < limiteRolagem) {
-            rolagemY += (velocidadeRolagem * 5f) * delta;
+            rolagemY += (velocidadeRolagem * 6f) * delta;
         }
 
-        // Volta ao menu após os créditos
+        // Voltar ao menu
         if (botaoSelecionado && esq && dir) {
             jogo.setScreen(new MenuScreen(jogo));
         }
 
         camera.update();
+
         batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
 
         float espacamentoY = rolagemY;
-        float largura      = Gdx.graphics.getWidth();
+        float largura = Gdx.graphics.getWidth();
 
-        // Bloco 1 – Criadores
-        fonteCargo.draw(batch, "Criadores", 0, espacamentoY, largura, Align.center, false);
-        fonteNome.draw(batch, "Arthur B. Freitag",     0, espacamentoY -  50, largura, Align.center, false);
-        fonteNome.draw(batch, "Arthur O. Deitos",      0, espacamentoY - 110, largura, Align.center, false);
-        fonteNome.draw(batch, "Artur D. Costa",        0, espacamentoY - 160, largura, Align.center, false);
-        fonteNome.draw(batch, "Bruno Darsie",          0, espacamentoY - 210, largura, Align.center, false);
-        fonteNome.draw(batch, "Felipe Biondo",         0, espacamentoY - 260, largura, Align.center, false);
-        fonteNome.draw(batch, "Gabriel A. Dieterich",  0, espacamentoY - 310, largura, Align.center, false);
-        fonteNome.draw(batch, "Miguel R. Hoff",        0, espacamentoY - 360, largura, Align.center, false);
+        // =====================================
+        // BLOCO 1 - CRIADORES
+        // =====================================
+
+        fonteCargo.draw(
+            batch,
+            cargoCriadores[jogo.idioma],
+            0,
+            espacamentoY,
+            largura,
+            Align.center,
+            false
+        );
+
+        fonteNome.draw(batch, "Arthur B. Freitag",    0, espacamentoY -  50, largura, Align.center, false);
+        fonteNome.draw(batch, "Arthur O. Deitos",     0, espacamentoY - 110, largura, Align.center, false);
+        fonteNome.draw(batch, "Artur D. Costa",       0, espacamentoY - 160, largura, Align.center, false);
+        fonteNome.draw(batch, "Bruno Darsie",         0, espacamentoY - 210, largura, Align.center, false);
+        fonteNome.draw(batch, "Felipe Biondo",        0, espacamentoY - 260, largura, Align.center, false);
+        fonteNome.draw(batch, "Gabriel A. Dieterich", 0, espacamentoY - 310, largura, Align.center, false);
+        fonteNome.draw(batch, "Miguel R. Hoff",       0, espacamentoY - 360, largura, Align.center, false);
+
         espacamentoY -= 500;
 
-        // Bloco 2 – Arte e Design
-        fonteCargo.draw(batch, "Arte e Design", 0, espacamentoY, largura, Align.center, false);
-        fonteNome.draw(batch, "Arthur O. Deitos",     0, espacamentoY -  50, largura, Align.center, false);
-        fonteNome.draw(batch, "Gabriel A. Dieterich", 0, espacamentoY - 100, largura, Align.center, false);
-        fonteNome.draw(batch, "Felipe Biondo",        0, espacamentoY - 150, largura, Align.center, false);
-        fonteNome.draw(batch, "Bruno Darsie",         0, espacamentoY - 200, largura, Align.center, false);
-        fonteNome.draw(batch, "Miguel R. Hoff",       0, espacamentoY - 250, largura, Align.center, false);
-        espacamentoY -= 380;
+        // =====================================
+        // BLOCO 2 - ARTE E DESIGN
+        // =====================================
 
-        // Bloco 3 – Programação
-        fonteCargo.draw(batch, "Programacao", 0, espacamentoY, largura, Align.center, false);
-        fonteNome.draw(batch, "Artur D. Costa",    0, espacamentoY -  50, largura, Align.center, false);
-        fonteNome.draw(batch, "Arthur B. Freitag", 0, espacamentoY - 100, largura, Align.center, false);
-        espacamentoY -= 220;
+        fonteCargo.draw(
+            batch,
+            cargoArteDesign[jogo.idioma],
+            0,
+            espacamentoY,
+            largura,
+            Align.center,
+            false
+        );
 
-        // Bloco 4 – Roteiro
-        fonteCargo.draw(batch, "Roteiro", 0, espacamentoY, largura, Align.center, false);
-        fonteNome.draw(batch, "Felipe Biondo", 0, espacamentoY -  50, largura, Align.center, false);
-        fonteNome.draw(batch, "Miguel R. Hoff", 0, espacamentoY - 100, largura, Align.center, false);
-        fonteNome.draw(batch, "Bruno Darsie",   0, espacamentoY - 150, largura, Align.center, false);
-        espacamentoY -= 250;
+        fonteNome.draw(batch, "Arthur B. Freitag",    0, espacamentoY -  50, largura, Align.center, false);
+        fonteNome.draw(batch, "Arthur O. Deitos",     0, espacamentoY - 110, largura, Align.center, false);
+        fonteNome.draw(batch, "Artur D. Costa",       0, espacamentoY - 160, largura, Align.center, false);
+        fonteNome.draw(batch, "Bruno Darsie",         0, espacamentoY - 210, largura, Align.center, false);
+        fonteNome.draw(batch, "Felipe Biondo",        0, espacamentoY - 260, largura, Align.center, false);
+        fonteNome.draw(batch, "Gabriel A. Dieterich", 0, espacamentoY - 310, largura, Align.center, false);
+        fonteNome.draw(batch, "Miguel R. Hoff",       0, espacamentoY - 360, largura, Align.center, false);
 
-        // Bloco 5 – Agradecimentos
-        fonteCargo.draw(batch, "Agradecimentos Especiais", 0, espacamentoY, largura, Align.center, false);
-        fonteNome.draw(batch, "Tramontina CEIT", 0, espacamentoY - 50, largura, Align.center, false);
+        espacamentoY -= 500;
+
+        // =====================================
+        // BLOCO 3 - PROGRAMACAO
+        // =====================================
+
+        fonteCargo.draw(
+            batch,
+            cargoProgramacao[jogo.idioma],
+            0,
+            espacamentoY,
+            largura,
+            Align.center,
+            false
+        );
+
+        fonteNome.draw(batch, "Arthur B. Freitag",    0, espacamentoY -  50, largura, Align.center, false);
+        fonteNome.draw(batch, "Arthur O. Deitos",     0, espacamentoY - 110, largura, Align.center, false);
+        fonteNome.draw(batch, "Artur D. Costa",       0, espacamentoY - 160, largura, Align.center, false);
+        fonteNome.draw(batch, "Bruno Darsie",         0, espacamentoY - 210, largura, Align.center, false);
+        fonteNome.draw(batch, "Felipe Biondo",        0, espacamentoY - 260, largura, Align.center, false);
+        fonteNome.draw(batch, "Gabriel A. Dieterich", 0, espacamentoY - 310, largura, Align.center, false);
+        fonteNome.draw(batch, "Miguel R. Hoff",       0, espacamentoY - 360, largura, Align.center, false);
+
+        espacamentoY -= 480;
+
+        // =====================================
+        // BLOCO 4 - ROTEIRO
+        // =====================================
+
+        fonteCargo.draw(
+            batch,
+            cargoRoteiro[jogo.idioma],
+            0,
+            espacamentoY,
+            largura,
+            Align.center,
+            false
+        );
+
+        fonteNome.draw(batch, "Arthur B. Freitag",    0, espacamentoY -  50, largura, Align.center, false);
+        fonteNome.draw(batch, "Arthur O. Deitos",     0, espacamentoY - 110, largura, Align.center, false);
+        fonteNome.draw(batch, "Artur D. Costa",       0, espacamentoY - 160, largura, Align.center, false);
+        fonteNome.draw(batch, "Bruno Darsie",         0, espacamentoY - 210, largura, Align.center, false);
+        fonteNome.draw(batch, "Felipe Biondo",        0, espacamentoY - 260, largura, Align.center, false);
+        fonteNome.draw(batch, "Gabriel A. Dieterich", 0, espacamentoY - 310, largura, Align.center, false);
+        fonteNome.draw(batch, "Miguel R. Hoff",       0, espacamentoY - 360, largura, Align.center, false);
+
+        espacamentoY -= 480;
+
+        // =====================================
+        // BLOCO 5 - AGRADECIMENTOS
+        // =====================================
+
+        fonteCargo.draw(
+            batch,
+            cargoAgradecimentos[jogo.idioma],
+            0,
+            espacamentoY,
+            largura,
+            Align.center,
+            false
+        );
+
+        fonteNome.draw(
+            batch,
+            "Tramontina CEIT",
+            0,
+            espacamentoY - 50,
+            largura,
+            Align.center,
+            false
+        );
+
         espacamentoY -= 400;
 
-        // Botão Voltar
-        Texture texturaAtual = botaoSelecionado ? btnVoltarSelect : btnVoltar;
-        batch.draw(texturaAtual, (largura / 2f) - 150f, espacamentoY, 300f, 150f);
+        // =====================================
+        // BOTAO VOLTAR
+        // =====================================
+
+        Texture texturaAtual =
+            botaoSelecionado ? btnVoltarSelect : btnVoltar;
+
+        batch.draw(
+            texturaAtual,
+            (largura / 2f) - 150f,
+            espacamentoY,
+            300f,
+            150f
+        );
 
         batch.end();
     }
 
-    public void atualizarTexturasIdioma(){
+    public void atualizarTexturasIdioma() {
 
-        if (jogo.idioma == 0){
-            btnVoltar       = new Texture("BotaoVoltar.png");
+        if (jogo.idioma == 0) {
+
+            btnVoltar = new Texture("BotaoVoltar.png");
             btnVoltarSelect = new Texture("BotaoVoltarSelect.png");
-        }else if (jogo.idioma == 1){
-            btnVoltar       = new Texture("BotaoVoltar_ES.png");
+
+        } else if (jogo.idioma == 1) {
+
+            btnVoltar = new Texture("BotaoVoltar_ES.png");
             btnVoltarSelect = new Texture("BotaoVoltarSelect_ES.png");
-        }else{
-            btnVoltar       = new Texture("BotaoVoltar_EN.png");
+
+        } else {
+
+            btnVoltar = new Texture("BotaoVoltar_EN.png");
             btnVoltarSelect = new Texture("BotaoVoltarSelect_EN.png");
         }
     }
 
-    @Override public void resize(int width, int height) { viewport.update(width, height, true); }
-    @Override public void show() {}
-    @Override public void pause() {}
-    @Override public void resume() {}
-    @Override public void hide() {}
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
 
     @Override
     public void dispose() {
+
         batch.dispose();
+
         fonteCargo.dispose();
         fonteNome.dispose();
+
         btnVoltar.dispose();
         btnVoltarSelect.dispose();
     }
